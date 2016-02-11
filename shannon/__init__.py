23098a385cf9b2247b9643d43c3e7fd514cfd989
Niru Maheswaranathan <nirum@stanford.edu>
2016-02-10 21:01:10 -0800

    Adds version info to shannon/__init__.py, removes the unnecessary tests/__init__.py

---
+++ shannon/__init__.py
@@ -0,0 +1,9 @@
+"""
+Shannon
+
+A python package for computing the mutual information and entropy for continuous and discrete data.
+"""
+
+__version__ = '0.1.0'
+
+__all__ = ['continuous', 'bottleneck', 'discrete']
